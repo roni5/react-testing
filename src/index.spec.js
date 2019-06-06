16fb549f9b7c53f7fd77790836bd1dd623744a5c
roni5 <roniemail1@gmail.com>
2019-06-06 23:48:41 +0100

    snapshot jest -u

--- src/index.spec.js
+++ src/index.spec.js
@@ -1,17 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { shallow, configure} from 'enzyme';
+import { shallow, configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import App from './App';
 
 configure({ adapter: new Adapter() });
 
 describe('Examining the syntax of Jest tests', () => {
-
   it('sums numbers', () => {
-      expect(1 + 2).toEqual(3);
-      expect(2 + 2).toEqual(4);
-   });
+    expect(1 + 2).toEqual(3);
+    expect(2 + 2).toEqual(4);
+  });
 });
 
 // it('renders welcome message', () => {
